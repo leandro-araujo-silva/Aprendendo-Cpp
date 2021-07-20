9496187a75e5b8a5b26628772b4ee09cc2264add
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2021-07-19 22:53:47 -0300

    Update obstaculos-roma.cpp

--- Exercicios2/obstaculos-roma.cpp
+++ Exercicios2/obstaculos-roma.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void int_romano(int valor) {
   
-  char numeros[1 == 'I', 5 == 'V', 10 == 'X', 50 == 'L', 100 == 'C', 500 == 'D', 1000 == 'M'];
+  char numeros{1 == 'I', 5 == 'V', 10 == 'X', 50 == 'L', 100 == 'C', 500 == 'D', 1000 == 'M'};
 
   for(int i = 0; i < 7; i++) {
     if (valor==numeros[i]) {
